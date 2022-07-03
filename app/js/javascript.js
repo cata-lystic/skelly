2bb1da212e529b39a6e853ad6118cad8392bd978
cata-lystic <ntqtmslf@protonmail.com>
2022-07-03 01:08:46 -0800

    Added "Scroll To Top" arrow if page gets too large

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,4 +1,21 @@
 $("document").ready(function() {
 
+  // Scroll to top arrow
+  $(window).scroll(function(){
+
+      if ($(window).scrollTop() > 300){
+        // show back to top
+        $('#actionButton').css({opacity: 1})
+      } else{
+        // hide back to top
+        $('#actionButton').css({opacity: 0})
+      }
+
+  })
 
 })
+
+// Click FAB to go to top of the page
+$(document).on("click", "#actionButton, #brandText", function() {
+    $('html, body').animate({ scrollTop: 0 }, 500)
+});
